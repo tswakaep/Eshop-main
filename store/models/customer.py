4dc518c7b61eed6bd587c8fd6d99b1a392a7c1d9
tswakaep <phelanetm@gmail.com>
2024-09-11 11:26:14 +0200

    ecommerce
    Eshop

---
+++ store/models/customer.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField (max_length=50)
+    phone = models.CharField(max_length=10)
+    email=models.EmailField()
+    password = models.CharField(max_length=100)
+
+    #to save the data
+    def register(self):
+        self.save()
+
+
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email= email)
+        except:
+            return False
+    def isExists(self):
+        return Customer.objects.filter(email=self.email).exists()
+ 
+    # This will make the object representation more meaningful
+    def _str_(self):
+        return f"{self.first_name} {self.last_name} - {self.email}"
+ 
+    class Meta:
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+
+    
+    
